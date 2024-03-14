1b398ee14fca93182c23fac3d93d277f43860abd
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-03-14 12:47:10 -0300

    feat: Adding the Fibonacci sequence test.

---
+++ TargetSistemas/src/main/java/org/example/SequenceOfFibonacci.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SequenceOfFibonacci {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter the initial number of the Fibonacci sequence: ");
+    int numberToCheck = input.nextInt();
+
+    if (isFibonacci(numberToCheck)) {
+        System.out.println("The number " + numberToCheck + " belong to teh Fibonacci sequence.");
+    } else {
+        System.out.println("The number " + numberToCheck + " does not belong to the Fibonacci sequence.");
+    }
+
+    input.close();
+}
+
+    public static boolean isFibonacci(int number){
+        int a = 0;
+        int b = 1;
+
+        while (a <= number){
+            if (a == number){
+                return true;
+            }
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return false;
+    }
+}
